12781dde74f8dc168cb7637a05d8f80211f4ffa4
Álvaro Campos Vega <alvarocamposvega@gmail.com>
2018-09-26 21:37:13 +0200

    Ejercicios 9, 10, 11 y 12 del tema 3

---
+++ Tema_3/Ejercicio11.java
@@ -0,0 +1,20 @@
+/*
+*
+* EJERCICIO 11
+* conversor de Kb a Mb
+*
+*/
+
+public class Ejercicio11 {
+  public static void main(String[] args) {
+  
+	System.out.print("Introduzca los Kb que desea convertir: ");
+    int kiloB = Integer.parseInt(System.console().readLine());
+
+    
+    int conversion = 1000;
+    
+    int megaB = (kiloB / conversion);
+    System.out.println(kiloB + " Kb son " + megaB + " Mb");
+  }
+}
