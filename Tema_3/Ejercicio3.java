5d2e3723cfb0c83a319938c4f931ad93c16dd212
Álvaro Campos Vega <alvarocamposvega@gmail.com>
2018-09-27 08:49:07 +0200

    Corregido ejercicio 3 tema 3

--- Tema_3/Ejercicio3.java
+++ Tema_3/Ejercicio3.java
@@ -8,12 +8,12 @@
 public class Ejercicio3 {
   public static void main(String[] args) {
 
-	int conversion = 166;
+	double conversion = 166.386;
   
 	System.out.print("Introduzca las pesetas que desea convertir: ");
     int pesetas = Integer.parseInt(System.console().readLine());
 
-    int euros = (pesetas / conversion);
+    double euros = (double)(pesetas / conversion);
     System.out.print("Conversión realizada, usted recibe ");
    
 	// Si la operación da 1 muestra euro en vez de euros
