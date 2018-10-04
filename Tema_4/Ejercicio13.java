fccb5fc8e387a9abe0fea0e1e602a8c0c08b9a19
Álvaro Campos Vega <alvarocamposvega@gmail.com>
2018-10-04 09:04:19 +0200

    Ejercicio 14 completado y ejercicio 13 depurado

--- Tema_4/Ejercicio13.java
+++ Tema_4/Ejercicio13.java
@@ -24,88 +24,87 @@ public class Ejercicio13 {
     
     if ((num1 < num2) && (num1 < num3)) { // Si num1 es el menor
 
-      numMenor = num1;
+        numMenor = num1;
       
-      // Miramos cual de los otros dos es el mayor
-      if ((num2 > num1) && (num2 > num3)) {
+        // Miramos cual de los otros dos es el mayor
+        if ((num2 > num1) && (num2 > num3)) {
 
-        numMayor = num2;
-        num = num3;
+          numMayor = num2;
+          num = num3;
         
-      } else if ((num3 > num1) && (num3 > num2)) {
+        } else if ((num3 > num1) && (num3 > num2)) {
 
-        numMayor = num3;
-        num = num2;
-
-      } else { // Si los mayores son iguales
-
-          num = num2;
           numMayor = num3;
-      }
+          num = num2;
+
+        } else { // Si los mayores son iguales
+
+            num = num2;
+            numMayor = num3;
+        }
 
     } else if ((num2 < num1) && (num2 < num3)) { // Si num2 es el menor
 
-      numMenor = num2;
+        numMenor = num2;
       
-      // Miramos cual de los otros dos es el mayor
-      if ((num1 > num2) && (num1 > num3)) {
+        // Miramos cual de los otros dos es el mayor
+        if ((num1 > num2) && (num1 > num3)) {
 
-        numMayor = num1;
-        num = num3;
+          numMayor = num1;
+          num = num3;
         
-      } else if ((num3 > num1) && (num3 > num2)) {
+        } else if ((num3 > num1) && (num3 > num2)) {
 
-        numMayor = num3;
-        num = num1;
-
-      } else { // Si los mayores son iguales
-
-          num = num1;
           numMayor = num3;
-      }
+          num = num1;
+
+        } else { // Si los mayores son iguales
+
+            num = num1;
+            numMayor = num3;
+        }
 
     } else if ((num3 < num1) && (num3 < num2)) { // Si num3 es el menor
 
-      numMenor = num3;
+        numMenor = num3;
       
-      // Miramos cual de los otros dos es el mayor
-      if ((num2 > num1) && (num2 > num3)) {
+        // Miramos cual de los otros dos es el mayor
+        if ((num2 > num1) && (num2 > num3)) {
 
-        numMayor = num2;
-        num = num1;
+          numMayor = num2;
+          num = num1;
         
-      } else if ((num1 > num2) && (num1 > num3)) {
+        } else if ((num1 > num2) && (num1 > num3)) {
 
-        numMayor = num1;
-        num = num2;
+          numMayor = num1;
+          num = num2;
 
-      } else { // Si los mayores son iguales
+        } else { // Si los mayores son iguales
 
           num = num2;
           numMayor = num1;
-      }
+        }
       
     } else { // Si los menores son iguales
 
-      if (num1 == num2) {
+        if (num1 == num2) {
 
-        numMenor = num1;
-        num = num2;
-        numMayor = num3;
+          numMenor = num1;
+          num = num2;
+          numMayor = num3;
 
-      } else if (num1 == num3) {
+        } else if (num1 == num3) {
 
-        numMenor = num1;
-        num = num3;
-        numMayor = num2;
+          numMenor = num1;
+          num = num3;
+          numMayor = num2;
 
-      } else if (num2 == num3) {
+        } else if (num2 == num3) {
 
-        numMenor = num2;
-        num = num3;
-        numMayor = num1;
-
-      }
+          numMenor = num2;
+          num = num3;
+          numMayor = num1;
+        }
     }
     
     System.out.print("Como los has introducido: " + num1 + ", " + num2 + ", ");
