b364e998c285b95fca7ce261b6b04b15df0b9113
Álvaro Campos Vega <alvarocamposvega@gmail.com>
2018-10-18 08:42:15 +0200

    Ejercicios 31 y 32 del tema 5

---
+++ Tema_5/Ejercicio31.java
@@ -0,0 +1,30 @@
+/*
+*
+* EJERCICIO 31
+* pinta la letra L con asteriscos dada la altura
+*
+*/
+
+public class Ejercicio31 {
+  public static void main(String[] args) {
+    
+    System.out.print("Introduce la altura de la L: ");
+    int altura = Integer.parseInt(System.console().readLine());
+    System.out.println();
+    
+    int paloHor = (altura / 2) + 1;
+    
+    for (int i = 1; i < altura; i++) {
+      
+      System.out.println("*");
+      
+      if (i == (altura - 1)) {
+        
+        for (int j = 0; j < paloHor; j++) {
+          
+          System.out.print("* ");
+        }
+      }
+    }
+  }
+}
