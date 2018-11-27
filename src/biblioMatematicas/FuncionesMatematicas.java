a50ed9fd4a05956201e74a1a5efc30f7da649203
Alvaro Campos Vega <alvarocamposvega@gmail.com>
2018-11-27 10:56:19 +0100

    Funciones Matemáticas
    Ejercicios 7, 8, 9, 10, 11 y 12

--- src/biblioMatematicas/FuncionesMatematicas.java
+++ src/biblioMatematicas/FuncionesMatematicas.java
@@ -13,8 +13,8 @@ public class FuncionesMatematicas {
 	Scanner s = new Scanner(System.in);
     
     int numero = Integer.parseInt(s.nextLine());
-    int indice = Integer.parseInt(s.nextLine());
-    System.out.print(digitoN(numero, indice));
+    int digito = Integer.parseInt(s.nextLine());
+    System.out.print(pegaPorDelante(numero, digito));
   }
   
   // FUNCIONES #################################################################
@@ -60,7 +60,6 @@ public class FuncionesMatematicas {
   //EJERCICIO 3 #################################################################
   /**
    * Devuelve el siguiente número primo mayor que el número introducido
-   * 
    * @param Número entero
    * @return Número entero primo
    */
@@ -81,7 +80,6 @@ public class FuncionesMatematicas {
   //EJERCICIO 4 #################################################################
   /**
    * Dada una base y un exponente devuelve la potencia
-   * 
    * @param Número entero que es la base
    * @param Número entero que es el exponente
    * @return Un número entero que es la potencia
@@ -165,6 +163,98 @@ public class FuncionesMatematicas {
     int resultado = (numeroInv % cifra) / (cifra/10);
     
     return resultado;
+  }
+  
+  //EJERCICIO 8 #################################################################
+  /**
+   * Devuelve la posición de la primera ocurrencia de un dígito
+   * de un número entero
+   * @param Número entero que represena el dígito
+   * @return Un número entero que indica la posición del dígito
+   */
+  public static int posicionDeDigito(int numero, int digito) {
+	  
+	int digitosNumero = digitos(numero);
+	int digitoComp;
+	
+	for (int i = 0; i < digitosNumero; i++) {
+		
+		digitoComp = digitoN(numero, i);
+		
+		if (digitoComp == digito) {
+			
+			return i;
+		}
+	}
+	
+	return -1;
+  }
+  
+  //EJERCICIO 9 #################################################################
+  /**
+   * Le quita a un número N dígitos por detrás (por la derecha)
+   * @param Número entero
+   * @param Número entero que indica el número de dígitos
+   * @return El número sin los dígitos
+   */
+  public static int quitaPorDetras(int numero, int digitos) {
+    
+	int cifra = 1;
+	
+	for (int i = 0; i < digitos; i++) {
+		
+		cifra *= 10;
+	}
+	
+	return (numero / cifra);
+  }
+  
+  //EJERCICIO 10 #################################################################
+  /**
+   * Le quita a un número N dígitos por delante (por la izquierda)
+   * @param Número entero
+   * @param Número entero que indica el número de dígitos
+   * @return El número sin los dígitos
+   */
+  public static int quitaPorDelante(int numero, int digitos) {
+    
+	int numeroVolteado = voltea(numero);
+	int cifra = 1;
+		
+	for (int i = 0; i < digitos; i++) {
+			
+		cifra *= 10;
+	}
+		
+	return (voltea(numeroVolteado / cifra));
+  }
+
+  //EJERCICIO 11 #################################################################
+  /**
+   * Añade un dígito a un número por detrás
+   * @param Número entero
+   * @param Dígito que se quiere añadir
+   * @return El número con el dígito nuevo
+   */
+  public static int pegaPorDetras(int numero, int digito) {
+    
+	String numeroNuevo = String.valueOf(numero) + String.valueOf(digito);
+	
+	return Integer.parseInt(numeroNuevo);
+  }
+  
+  //EJERCICIO 12 #################################################################
+  /**
+   * Añade un dígito a un número por delante
+   * @param Número entero
+   * @param Dígito que se quiere añadir
+   * @return El número con el dígito nuevo
+   */
+  public static int pegaPorDelante(int numero, int digito) {
+    
+	String numeroNuevo = String.valueOf(digito) + String.valueOf(numero);
+	
+	return Integer.parseInt(numeroNuevo);
   }
   
   /**
