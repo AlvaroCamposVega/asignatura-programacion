ccc051e360324df54abc0e66c84117b8c387f7fd
AlvaroCamposVega <alvarocamposvega@gmail.com>
2019-01-24 08:34:27 +0100

    Ejercicio 8 tema 10

---
+++ src/tema10/ejercicio8/Ejercicio8.java
@@ -0,0 +1,50 @@
+package tema10.ejercicio8;
+
+import java.util.ArrayList;
+
+/*
+*
+* EJERCICIO 8
+* Realiza un programa que escoja al azar 10 cartas de la baraja española
+* (10 objetos de la clase Carta). Emplea un objeto de la clase ArrayList para
+* almacenarlas y asegúrate de que no se repite ninguna.
+*
+*/
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+
+		ArrayList<Carta> a = new ArrayList<Carta>();
+		Carta c = new Carta();
+		a.add(c);
+		boolean e;
+		
+		for (int i = 0; i < 9; i++) {
+			
+			e = true;
+			
+			while (e) { // Hacemos que la nueva carta no sea repetida
+				
+				e = false;
+				c = new Carta();
+				
+				for (Carta b : a) {
+					
+					if (b.equals(c)) {
+						
+						e = true;
+					}
+				}
+			}
+			
+			a.add(c);
+		}
+		
+		for (Carta b : a) {
+			
+			System.out.println(b);
+		}
+	}
+
+}
