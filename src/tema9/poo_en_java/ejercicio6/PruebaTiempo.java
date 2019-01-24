b9ba879a4f5691e69c79f7d1a9e43c6057ce9f74
AlvaroCamposVega <alvarocamposvega@gmail.com>
2019-01-24 08:35:05 +0100

    Ejercicio 6 tema 9 incompleto

---
+++ src/tema9/poo_en_java/ejercicio6/PruebaTiempo.java
@@ -0,0 +1,10 @@
+package tema9.poo_en_java.ejercicio6;
+
+public class PruebaTiempo {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
---
+++ src/tema9/poo_en_java/ejercicio6/Tiempo.java
@@ -0,0 +1,30 @@
+package tema9.poo_en_java.ejercicio6;
+
+public class Tiempo {
+	
+	private int horas;
+	private int minutos;
+	private int segundos;
+
+	public Tiempo(int horas, int minutos, int segundos) {
+
+		
+	}
+	
+	public int suma(Tiempo tiempo) {
+		
+		return 1;
+	}
+	
+	public int resta(Tiempo tiempo) {
+		
+		return 1;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "";
+	}
+
+}
