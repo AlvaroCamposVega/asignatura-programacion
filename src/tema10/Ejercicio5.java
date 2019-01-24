85eab12eedab4dbfa99a0c854533fd4c9e724135
AlvaroCamposVega <alvarocamposvega@gmail.com>
2019-01-24 08:32:51 +0100

    Ejercicio 5 tema 10 incompleto

---
+++ src/tema10/Ejercicio5.java
@@ -0,0 +1,21 @@
+package tema10;
+
+/*
+*
+* EJERCICIO 5
+* Realiza de nuevo el ejercicio de la colección de discos pero utilizando esta vez
+* una lista para almacenar la información sobre los discos en lugar de un array
+* convencional. Comprobarás que el código se simplifica notablemente ¿Cuánto
+* ocupa el programa original hecho con un array? ¿Cuánto ocupa este nuevo
+* programa hecho con una lista?
+*
+*/
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
